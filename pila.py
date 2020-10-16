82450b6ea3af1ebeb72169c883bb566c488e5805
Luis-Solano97 <72114946+Luis-Solano97@users.noreply.github.com>
2020-10-15 23:36:27 -0500

    pila

--- pila.py
+++ pila.py
@@ -1,2 +1,15 @@
-pila(3.5.4)
-print ("pila")
+pila(3.5.4) #inicio arreglo de una pila
+print ("pila") #impresion de la pila
+#345
+
+pila.append(7)
+pila.append(8)
+
+print (pila)
+#34578
+
+print ("saco es elemento",pila.pop())
+#8
+
+print ("queda la pila asi", pila)
+#3457
