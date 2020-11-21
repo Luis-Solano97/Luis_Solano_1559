33b384d6ac716d285530560ed2ddbed36d486319
Luis-Solano97 <72114946+Luis-Solano97@users.noreply.github.com>
2020-11-21 01:01:27 -0600

    tarea7

---
+++ Tareas/Tarea-7_Practica-FirmaHash/Comandos.c
@@ -0,0 +1,15 @@
+Comandos git
+
+Git branch -a				Lista ramas
+Git checkout NombreRama			Cambia de rama
+Git init 				Nuevo repositorio (local)
+Cat -n NombreArchivo			Contenido del archivo con cuantas líneas
+History -c 				Borrar el history
+Git merge NOmbreDeLaRama		Une las ramas
+Git log					Para ver que se ha hecho
+pwd					Ruta absoluta
+Grep -h					Buscador en un archivo
+Rm -r					Eliminar archivo
+Mv NombreOriginal NombreNuevo		Renombrar
+Zip NombreCarpeta.zip			comprimir
+
